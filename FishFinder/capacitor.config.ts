eaeeb053fefb7a2fda97a632c6527ac8bb909b1e
William Vernaschi <williamvernaschi@gmail.com>
2024-06-17 19:53:05 -0300

    arruma nome do app

--- FishFinder/capacitor.config.ts
+++ FishFinder/capacitor.config.ts
@@ -2,12 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'FishFinder',
+  appName: 'Smart Fish Finder',
   webDir: 'www',
-  "plugins": {
-    "LocalNotifications": {},
-    "Http": {}
-  }
 };
 
 export default config;
